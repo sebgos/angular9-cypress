274d54124ea980ffe8689333c5cb09e002c8fa9b
Sebastian Gos <gos.sebastian@gmail.com>
2020-03-24 01:49:48 +0100

    more advanced automated test

--- cypress/integration/spec.ts
+++ cypress/integration/spec.ts
@@ -1,4 +1,17 @@
-it('smoke test', () => {
-  cy.visit('/');
-  cy.contains('angular9-cypress app is running!');
+describe('When Angular starting page is loaded', () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('has app title, shows proper command by default and reacts on a command changes', () => {
+    cy.contains('angular9-cypress');
+
+    cy.contains('.terminal', 'ng generate component xyz');
+
+    cy.contains('Angular Material').click();
+    cy.contains('.terminal', 'ng add @angular/material');
+
+    cy.contains('Run and Watch Tests').click();
+    cy.contains('.terminal', 'ng test');
+  });
 });
